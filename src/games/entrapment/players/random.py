680cda0d08122384de554d7c72831dae27fd54fa
gp-debora <131462437+gp-debora@users.noreply.github.com>
2023-05-09 14:09:58 +0100

    Atualização

--- src/games/entrapment/players/random.py
+++ src/games/entrapment/players/random.py
@@ -12,7 +12,12 @@ class RandomEntrapmentPlayer(EntrapmentPlayer):
         super().__init__(name)
 
     def get_action(self, state: EntrapmentState):
-        return EntrapmentAction(randint(0, state.get_num_cols()), randint(0, state.get_num_rows()))
+        #return EntrapmentAction(randint(0, state.get_num_cols()), randint(0, state.get_num_rows()))
+        num_cols = state.get_num_cols()
+        num_rows = state.get_num_rows()
+        new_col = randint(0, num_cols - 1)
+        new_row = randint(0, num_rows - 1)
+        return EntrapmentAction(new_col, new_row)
 
     def event_action(self, pos: int, action, new_state: State):
         # ignore
@@ -21,3 +26,5 @@ class RandomEntrapmentPlayer(EntrapmentPlayer):
     def event_end_game(self, final_state: State):
         # ignore
         pass
+
+
